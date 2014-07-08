c85f2f4ca48d8f493187fe84ae5c2f702825d616
Zafar Takhirov <z.tahirov@gmail.com>
2014-07-08 00:03:24 -0700

    AUTOCOMMIT: Tue Jul  8 00:03:24 PDT 2014

---
+++ Arrays and Sorting/The Full Counting Sort/solution.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+#define RANGE 100
+int C[RANGE] = {0};
+int main() {
+    // int k = RANGE;  // counter size
+    int N;  // Number of elements in an array;
+    
+    cin >> N;
+    
+    // Get unsorrted array:
+    short A[N];
+    string str[N];
+    
+    for (int i = 0; i < N; i++){
+        cin >> A[i] >> str[i];
+        C[A[i]]++;
+        if (i < N/2)
+            str[i] = "-";
+    }
+    
+    for (int i = 1; i < RANGE; i++) {
+        C[i] += C[i-1];
+    }
+
+    // Create sorted array:
+    // short B[N];
+    string strB[N];
+    
+    for (int i = N-1; i >= 0; i--) {
+        // cout << N << ' ' << i << endl;
+        C[A[i]] -= 1;
+        // B[C[A[i]]] = A[i];
+        strB[C[A[i]]] = str[i];
+    } 
+    
+    
+    for (int i = 0; i < N; i++) {
+        cout << strB[i] << " ";
+    }
+    
+}
