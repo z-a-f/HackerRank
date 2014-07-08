789d070eebbf0b2832f7617f894f6abdf84cc36e
Zafar Takhirov <z.tahirov@gmail.com>
2014-07-08 01:03:08 -0700

    AUTOCOMMIT: Tue Jul  8 01:03:08 PDT 2014

--- Arrays and Sorting/The Full Counting Sort/solution_incomplete.cpp
+++ Arrays and Sorting/The Full Counting Sort/solution_incomplete.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 #define RANGE 100
+#define WORDLEN 10
+
 
 int main() {
     // int k = RANGE;  // counter size
@@ -12,28 +14,42 @@ int main() {
     
     // Get unsorrted array:
     short A[N];
-    string str[N];
+    // string str[N];
+    char str[N/2][WORDLEN];
+    char temp[1][WORDLEN];
+    
     int C[RANGE] = {0};
     for (int i = 0; i < N; i++){
-        cin >> A[i] >> str[i];
+        cin >> A[i] >> temp[0];
         C[A[i]]++;
+        if (i >= N/2) {
+        	strcpy(str[i-N/2], temp[0]);
+        }
+        /*
         if (i < N/2)
-            str[i] = "-";
+            //str[i] = "-";
+            strcpy(str[i], "-");
+        */
     }
     
-    for (unsigned short i = 1; i < RANGE; i++) {
+    for (int i = 1; i < RANGE; i++) {
         C[i] += C[i-1];
     }
 
     // Create sorted array:
     // short B[N];
-    string strB[N];
+    // string strB[N];
+    char strB[N][WORDLEN];
     
     for (int i = N-1; i >= 0; i--) {
         // cout << N << ' ' << i << endl;
         C[A[i]] -= 1;
         // B[C[A[i]]] = A[i];
-        strB[C[A[i]]] = str[i];
+        // strB[C[A[i]]] = str[i];
+        if (i >= N/2)
+	        strcpy(strB[C[A[i]]], str[i-N/2]);
+	    else
+	    	strcpy(strB[C[A[i]]], "-");
         
     }
     
